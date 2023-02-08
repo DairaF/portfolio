fb06b65909015b7cdeeb2630e1af25a64dd70010
DairaF <dairafaz@gmail.com>
2023-02-08 20:05:35 -0300

    browserrouter con basename

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import './App.css';
 import {
-  HashRouter,
   BrowserRouter,
   Routes,
   Route
@@ -11,14 +10,13 @@ import Project from './routes/Proyecto';
 function App() {
   return (
     <div className="App">
-        <HashRouter>
+        <BrowserRouter basename='/portfolio'>
           <Routes>
             <Route path="/" element={<Landing/>} />
             <Route path="/proyectos/" element={<Project/>} />
             <Route path="/proyectos/:id" element={<Project/>} />
-            <Route path="/:id" element={<Project/>} />
           </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
