e2e18c0ae2d61099c0a8b8cf1d538cdb5875a0d3
DairaF <dairafaz@gmail.com>
2023-02-08 21:49:54 -0300

    todo en landing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import {
   Route
 } from "react-router-dom";
 import Landing from './routes/Landing';
-import Dos from './routes/Dos';
 import Project from './routes/Proyecto';
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
         <HashRouter>
           <Routes>
             <Route path="/" element={<Landing/>} />
-            <Route path="/dos" element={<Dos/>} />
             <Route path="/proyectos/" element={<Project/>} />
             <Route path="/proyectos/:id" element={<Project/>} />
           </Routes>
--- src/routes/Landing copy.jsx
+++ src/routes/Landing copy.jsx
@@ -15,18 +15,23 @@
 // }
 // export default Landing;
 import React from 'react';
+import  Carousel  from '../components/carousel/Carousel'
 
 
-function Dos() {
+function Landing() {
 
 
     return (
         <div>
             <div id='home' className='typewriter child'>
                 <h1 className='fadeInUp'>Daira</h1>
+                <h2>creative <span id='developer'>developer</span></h2>
+            </div>
+            <div className='child'>
+                <Carousel/>
             </div>
         </div>
   );
 }
 
-export default Dos;
+export default Landing;
